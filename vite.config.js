384d2abf3bc8f6ec0ac3fa5a7b0fef84594d6817
Felipe Muñoz <f.munozg@uniandes.edu.co>
2025-03-30 20:32:05 -0500

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-server: {
-    allowedHosts: ['web-logistics-143596276526.us-central1.run.app', 'web-purchases-143596276526.us-central1.run.app', 'web-sales-143596276526.us-central1.run.app', 'web-sellers-143596276526.us-central1.run.app']
-  }
+export default {
+    // config options
+
+    server: {
+        allowedHosts: ['web-logistics-143596276526.us-central1.run.app', 'web-purchases-143596276526.us-central1.run.app', 'web-sales-143596276526.us-central1.run.app', 'web-sellers-143596276526.us-central1.run.app']
+    }
+
+}
