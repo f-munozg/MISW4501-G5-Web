0a2738c493b1c5f2c2c5a3da7fc2fcd1258d43c3
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-05 17:51:29 -0500

    036 - se hace merge con ramas de funcionalidades y se corrigen el error de async en los .spec.ts

--- src/app/shared/table-template/table-template.component.spec.ts
+++ src/app/shared/table-template/table-template.component.spec.ts
@@ -1,15 +1,15 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { TableTemplateComponent } from './table-template.component';
 
 describe('TableTemplateComponent', () => {
-  let component: TableTemplateComponent;
-  let fixture: ComponentFixture<TableTemplateComponent>;
+  let component: TableTemplateComponent<any>;
+  let fixture: ComponentFixture<TableTemplateComponent<any>>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TableTemplateComponent ]
     })
