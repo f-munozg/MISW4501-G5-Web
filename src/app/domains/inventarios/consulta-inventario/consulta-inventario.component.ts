bb939dd3c62380ad8127e6fc25f9ac4cfcea7832
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-05 00:17:58 -0500

    029 - Se modifica el campo de categoría para que sea un dropdown.

--- src/app/domains/inventarios/consulta-inventario/consulta-inventario.component.ts
+++ src/app/domains/inventarios/consulta-inventario/consulta-inventario.component.ts
@@ -8,34 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsultaInventarioComponent implements OnInit {
 
-    tableData = [
+  tableData = [
 
-    ]
+  ]
 
-    tableColumns = [
-      { 
-        name: 'bodega', 
-        header: 'Bodega', 
-        cell: (item: any) => `${item.bodega}` 
-      },
-      { 
-        name: 'unidades', 
-        header: 'Stock (Unidades)', 
-        cell: (item: any) => item.unidades 
-      },
-      { 
-        name: 'fecha_reposicion', 
-        header: 'Fecha Estimada Reposición', 
-        cell: (item: any) => item.role.toUpperCase() 
-      },
-      { 
-        name: 'ultima_actualizacion', 
-        header: 'Última Actualización', 
-        cell: (item: any) => item.role.toUpperCase() 
-      },
-    ];
+  tableColumns = [
+    { 
+      name: 'bodega', 
+      header: 'Bodega', 
+      cell: (item: any) => `${item.bodega}` 
+    },
+    { 
+      name: 'unidades', 
+      header: 'Stock (Unidades)', 
+      cell: (item: any) => item.unidades 
+    },
+    { 
+      name: 'fecha_reposicion', 
+      header: 'Fecha Estimada Reposición', 
+      cell: (item: any) => item.role.toUpperCase() 
+    },
+    { 
+      name: 'ultima_actualizacion', 
+      header: 'Última Actualización', 
+      cell: (item: any) => item.role.toUpperCase() 
+    },
+  ];
   
-    visibleColumns = ['bodega', 'unidades', 'fecha_reposicion', 'ultima_actualizacion'];
+  visibleColumns = ['bodega', 'unidades', 'fecha_reposicion', 'ultima_actualizacion'];
+
+  selectedValue!: string; // Debe ser revisado, selectedValue es temporal
+
+  categorias: any[] = []; // any debe ser cambiado cuando se implemente el servicio del cual lea.
 
   constructor() { }
 
