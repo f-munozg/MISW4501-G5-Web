027d21480204c8ea011cce653eb9372d403a4539
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-09 23:05:03 -0500

    064 - Se ajusta el botón de consulta de inventario en el componente de consulta de producto en bodega para añadir routing que es su funcionalidad. Revisar cuando se hagan cambios a app-routing.module.ts

--- src/app/domains/inventarios/inventarios.module.ts
+++ src/app/domains/inventarios/inventarios.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { DefaultWindowComponent } from 'src/app/shared/default-window/default-window.component';
 import { TableTemplateComponent } from 'src/app/shared/table-template/table-template.component';
@@ -21,7 +22,8 @@ import { ConsultaProductoBodegaComponent } from './consulta-producto-bodega/cons
     ReactiveFormsModule,
     HttpClientModule,
     DefaultWindowComponent,
-    TableTemplateComponent
+    TableTemplateComponent,
+    RouterModule
   ]
 })
 export class InventariosModule { }
