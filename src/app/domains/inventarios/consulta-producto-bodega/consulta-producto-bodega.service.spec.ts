d71c546bbbd38dd68a9d5054b50eb9c48e068290
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-13 20:09:24 -0500

    074 - Se actualizan parámetros para las pruebas del componente de consulta de producto en bodega

--- src/app/domains/inventarios/consulta-producto-bodega/consulta-producto-bodega.service.spec.ts
+++ src/app/domains/inventarios/consulta-producto-bodega/consulta-producto-bodega.service.spec.ts
@@ -2,15 +2,24 @@
 
 import { TestBed, waitForAsync, inject } from '@angular/core/testing';
 import { ConsultaProductoBodegaService } from './consulta-producto-bodega.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('Service: ConsultaProductoBodega', () => {
+  let service: ConsultaProductoBodegaService;
+  let httpMock: HttpTestingController;
+  let apiUrlWarehouses = '';
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ConsultaProductoBodegaService]
     });
+
+    service = TestBed.inject(ConsultaProductoBodegaService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should ...', inject([ConsultaProductoBodegaService], (service: ConsultaProductoBodegaService) => {
+  it('should ...', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
