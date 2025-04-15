9ab87644ae2bbaf75ae23a63ee7862262fcd3e02
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-14 22:20:53 -0500

    080 - Se añaden archivos de variables de ambiente dev y prod

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    apiUrlCustomers: 'https://backend-customers-143596276526.us-central1.run.app',
+    apiUrlOrders: 'https://backend-orders-143596276526.us-central1.run.app',
+    apiUrlProducts: 'https://backend-products-143596276526.us-central1.run.app',
+    apiUrlProviders: 'https://backend-providers-143596276526.us-central1.run.app',
+    apiUrlReports: 'https://backend-reports-143596276526.us-central1.run.app',
+    apiUrlRoutes: 'https://backend-routes-143596276526.us-central1.run.app',
+    apiUrlSales: 'https://backend-routes-143596276526.us-central1.run.app',
+    apiUrlSellers: 'https://backend-sellers-143596276526.us-central1.run.app',
+    apiUrlStock: 'https://backend-stock-143596276526.us-central1.run.app',
+    apiUrlUsers: 'https://backend-users-143596276526.us-central1.run.app'
+};
