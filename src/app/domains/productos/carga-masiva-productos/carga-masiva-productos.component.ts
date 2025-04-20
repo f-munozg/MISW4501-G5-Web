281b89fb9e96656af817fd63fb521159cb88cc52
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-20 02:59:47 -0500

    101 - Se trabaja en la implementación de la carga masiva de productos.

--- src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.component.ts
+++ src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CargaMasivaProductosService } from './carga-masiva-productos.service';
 import { Fabricante, FabricantesResponse } from '../producto.model';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-carga-masiva-productos',
@@ -11,6 +12,10 @@ import { Fabricante, FabricantesResponse } from '../producto.model';
 })
 export class CargaMasivaProductosComponent implements OnInit {
   cargaMasivaProductosForm!: FormGroup;
+  selectedFile: File | null = null;
+  errorMessage: string = '';
+  allowedExtensions = ['.csv', '.xlsx', '.xls'];
+  isSubmitting = false;
   listaFabricantes: Fabricante[] = [];
 
   constructor(
@@ -28,9 +33,105 @@ export class CargaMasivaProductosComponent implements OnInit {
         this.listaFabricantes = response.providers;
       },
       error: (err) => {
-        console.error('Error loading providers:', err);
+        console.error('Error cargando fabricantes:', err);
       }
     });
   }
+
+  onFileSelected(event: any): void {
+    this.errorMessage = '';
+    const file: File = event.target.files[0];
+    
+    if (!file) {
+      return;
+    }
+
+    const fileExt = file.name.substring(file.name.lastIndexOf('.')).toLowerCase();
+    if (!this.allowedExtensions.includes(fileExt)) {
+      this.errorMessage = 'Archivo inválido. Solo subir archivos CSV o Excel.';
+      this.selectedFile = null;
+      return;
+    }
+
+    this.selectedFile = file;
+  }
+
+  async uploadFile(formData: { productsFile: File, fieldFabricante: string }) {
+    try {
+      const upload$ = this.apiService.postData(formData);
+      
+      await new Promise((resolve, reject) => {
+        upload$.subscribe({
+          next: (event) => {
+            if (event.type === HttpEventType.Response) {
+              console.log('¡Carga exitosa!', event.body);
+              resolve(event.body);
+            }
+          },
+          error: (err) => {
+            this.errorMessage = 'La carga ha fallado: ' + err.message;
+            reject(err);
+          }
+        });
+      });
+    } catch (err) {
+      if (err instanceof Error) {
+        this.errorMessage = 'Error preparando la carga: ' + err.message;
+      } else {
+        this.errorMessage = 'Un error desconocido ha sucedido';
+      }
+      throw err; 
+    }
+  }
+
+  async onSubmit() {
+    if (this.cargaMasivaProductosForm.invalid) {
+      return;
+    }
+
+    if (!this.selectedFile) {
+      this.errorMessage = 'Por favor seleccione un archivo.';
+      return;
+    }
+
+    if (!this.isSubmitting) {
+      this.isSubmitting = true;
+      this.errorMessage = '';
+  
+      try {
+        const formData = {
+          ...this.cargaMasivaProductosForm.value,
+          productsFile: this.selectedFile
+        };
+  
+        await this.uploadFile(formData);
+        
+        console.log('Proceso completado exitosamente');
+        
+      } catch (error) {
+        // Para garantizar la seguridad de los tipos.
+        this.errorMessage = error instanceof Error 
+        ? `Error: ${error.message}`
+        : 'Error desconocido';
+      } finally {
+        this.isSubmitting = false; 
+    }
+    }
+  }
+
+  clearAll(){
+    this.cargaMasivaProductosForm.reset()
+
+    this.selectedFile = null;
+
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.value = '';
+    }
+
+    this.errorMessage = '';
+    this.isSubmitting = false;
+  }
+
 
 }
--- src/app/domains/productos/productos.module.ts
+++ src/app/domains/productos/productos.module.ts
@@ -3,19 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { DefaultWindowComponent } from 'src/app/shared/default-window/default-window.component';
 import { TableTemplateComponent } from 'src/app/shared/table-template/table-template.component';
 
 import { CargaProductoComponent } from './carga-producto/carga-producto.component';
 import { CargaMasivaProductosComponent } from './carga-masiva-productos/carga-masiva-productos.component';
-import { RouterModule } from '@angular/router';
-
 
 @NgModule({
   declarations: [
     CargaProductoComponent,
-    CargaMasivaProductosComponent
+    CargaMasivaProductosComponent,
   ],
   imports: [
     CommonModule,
