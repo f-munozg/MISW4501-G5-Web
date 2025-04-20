5eeca2cf91f8ec992a864f08bf724c048e1187f8
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-20 18:13:34 -0500

    104 - Se agregan pruebas para el servicio del componente de carga masiva de productos

--- src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.component.spec.ts
+++ src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.component.spec.ts
@@ -2,18 +2,30 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CargaMasivaProductosComponent } from './carga-masiva-productos.component';
+import { CargaMasivaProductosService } from './carga-masiva-productos.service';
+import { environment } from '../../../../environments/environment'
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductosModule } from '../productos.module';
 
 describe('CargaMasivaProductosComponent', () => {
   let component: CargaMasivaProductosComponent;
   let fixture: ComponentFixture<CargaMasivaProductosComponent>;
+  let service: CargaMasivaProductosService;
+  let httpMock: HttpTestingController;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ CargaMasivaProductosComponent ]
+      declarations: [ CargaMasivaProductosComponent ],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, ProductosModule],
+      providers: [ CargaMasivaProductosService ]
     })
     .compileComponents();
+
+    service = TestBed.inject(CargaMasivaProductosService);
+    httpMock = TestBed.inject(HttpTestingController);
   }));
 
   beforeEach(() => {
@@ -24,5 +36,8 @@ describe('CargaMasivaProductosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    const req = httpMock.expectOne(environment.apiUrlProviders + `/providers`);
+    req.flush({});
   });
 });
