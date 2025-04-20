2a88b193417b13c28dbd7df5185b707ba615eada
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-19 23:58:57 -0500

    100 - Se agrega un dropdown para escoger el proveedor ya que el backend requiere el proveedor para procesar exitosamente el archivo adjunto.

--- src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.component.ts
+++ src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CargaMasivaProductosService } from './carga-masiva-productos.service';
+import { Fabricante, FabricantesResponse } from '../producto.model';
 
 @Component({
   selector: 'app-carga-masiva-productos',
@@ -7,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carga-masiva-productos.component.css']
 })
 export class CargaMasivaProductosComponent implements OnInit {
+  cargaMasivaProductosForm!: FormGroup;
+  listaFabricantes: Fabricante[] = [];
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: CargaMasivaProductosService
+  ) { }
 
   ngOnInit() {
+    this.cargaMasivaProductosForm = this.formBuilder.group({
+      fieldFabricante: ['', Validators.required]
+    })
+
+    this.apiService.getListaFabricantes().subscribe({
+      next: (response: FabricantesResponse) => {
+        this.listaFabricantes = response.providers;
+      },
+      error: (err) => {
+        console.error('Error loading providers:', err);
+      }
+    });
   }
 
 }
--- src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.service.ts
+++ src/app/domains/productos/carga-masiva-productos/carga-masiva-productos.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FabricantesResponse } from '../producto.model';
+import { environment } from '../../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CargaMasivaProductosService {
 
-constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getListaFabricantes():
+  Observable<FabricantesResponse> {
+    return this.http.get<FabricantesResponse>(environment.apiUrlProviders + `/providers`); // Aquí va la URL del endpoint de Mateo G.
+  }
 }
