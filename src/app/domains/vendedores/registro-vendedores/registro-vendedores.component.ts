a79b38fd97a5f1159cbe42d704d799e77dba8f80
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-23 21:58:50 -0500

    113 - Se implementa un nuevo form para la consulta de vendedores y se agrega un botón para cambiar entre el form de registrar vendedores y de consultar vendedores.

--- src/app/domains/vendedores/registro-vendedores/registro-vendedores.component.ts
+++ src/app/domains/vendedores/registro-vendedores/registro-vendedores.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ZonaType2LabelMapping, ZonaVendedor } from '../vendedores.model';
+import { Vendedor, VendedoresResponse, ZonaType2LabelMapping, ZonaVendedor } from '../vendedores.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RegistroVendedoresService } from './registro-vendedores.service';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-registro-vendedores',
@@ -11,7 +12,14 @@ import { RegistroVendedoresService } from './registro-vendedores.service';
 })
 export class RegistroVendedoresComponent implements OnInit {
   registroVendedoresForm!: FormGroup;
-  isCreatingNewSeller = true;
+  consultaVendedoresForm!: FormGroup;
+
+  listaVendedores: Vendedor[] = [];
+  numerosIdentificacionFiltrados!: Observable<number[]>;
+
+  creandoNuevoVendedor = true;
+
+  isInViewMode: boolean = false;
 
   public ZonaType2LabelMapping = ZonaType2LabelMapping;
   public zonaTypes = Object.values(ZonaVendedor);
@@ -48,7 +56,7 @@ export class RegistroVendedoresComponent implements OnInit {
     private apiService: RegistroVendedoresService
   ) { }
 
-  private initializeForm(): void {
+  private initializeForms(): void {
     this.registroVendedoresForm = this.formBuilder.group({
       fieldNumeroIdentificacion: ['', Validators.required],
       fieldNombre: [ '', Validators.required ],
@@ -56,14 +64,63 @@ export class RegistroVendedoresComponent implements OnInit {
       fieldDireccion: [ '', Validators.required ],
       fieldTelefono: [ '', Validators.required ],
       fieldZona: [ '', Validators.required ]
-    })
+    });
+
+    this.consultaVendedoresForm = this.formBuilder.group({
+      fieldNumeroIdentificacion: ['', Validators.required],
+      fieldNombre: [ '', Validators.required ],
+      fieldCorreoElectronico: [ '', [Validators.required, Validators.email] ],
+      fieldDireccion: [ '', Validators.required ],
+      fieldTelefono: [ '', Validators.required ],
+      fieldZona: [ '', Validators.required ]
+    });
   }
 
   ngOnInit() {
-    this.initializeForm();
+    this.initializeForms();
+    this.autoCompletar();
+    this.cargarVendedores();
   }
 
-  private createNewSeller(): void {
+  private autoCompletar(): void {
+    this.numerosIdentificacionFiltrados = this.consultaVendedoresForm.get('fieldNumeroIdentificacion')!.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filtrarNumerosIdentificacion(value))
+    )
+  }
+
+  private _filtrarNumerosIdentificacion(value: string): number[] {
+    const valorFiltro = value?.toString().toLowerCase() || '';
+    return this.listaVendedores
+      .filter(vendedor => vendedor.identification_number.toString().includes(valorFiltro))
+      .map(vendedor => vendedor.identification_number)
+  }
+
+  private cargarVendedores(): void {
+    this.apiService.getListaVendedores().subscribe({
+      next: (response: VendedoresResponse) => {
+        this.listaVendedores = response.sellers;
+      },
+      error: (err) => {
+        console.error('Error loading sellers: ', err);
+      }
+    });
+  }
+
+  conNumeroIdentificacionSeleccionado(numeroIdentificacionSeleccionado: number): void {
+    const vendedor = this.listaVendedores.find(v => v.identification_number === numeroIdentificacionSeleccionado);
+    if (vendedor) {
+      this.consultaVendedoresForm.patchValue({
+        fieldNombre: vendedor.name,
+        fieldCorreoElectronico: vendedor.email,
+        fieldDireccion: vendedor.address,
+        fieldTelefono: vendedor.phone,
+        fieldZona: vendedor.zone
+      })
+    }
+  }
+
+  private crearNuevoVendedor(): void {
     const formData = this.registroVendedoresForm.value;
 
     this.apiService.postData(formData).subscribe({
@@ -77,6 +134,12 @@ export class RegistroVendedoresComponent implements OnInit {
     });
   }
 
+  toggleMode(): void {
+    this.isInViewMode = !this.isInViewMode;
+    if (!this.isInViewMode) {
+      this.consultaVendedoresForm.reset(); // Clear view form when switching back to add mode
+    }
+  }
 
   onSubmit(){
     if (this.registroVendedoresForm.invalid) {
@@ -84,8 +147,8 @@ export class RegistroVendedoresComponent implements OnInit {
       return;
     }
 
-    if (this.isCreatingNewSeller) {
-      this.createNewSeller()
+    if (this.creandoNuevoVendedor) {
+      this.crearNuevoVendedor()
     }
   }
 
