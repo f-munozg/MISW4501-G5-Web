5f09f562628fdd7bf3c5ce5e7f441bab6b64eb1e
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-04-25 21:05:39 -0500

    128 - Se inicializa componente de gestión de portafolio

---
+++ src/app/domains/fabricantes/gestion-portafolio/gestion-portafolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-portafolio',
+  templateUrl: './gestion-portafolio.component.html',
+  styleUrls: ['./gestion-portafolio.component.css']
+})
+export class GestionPortafolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
