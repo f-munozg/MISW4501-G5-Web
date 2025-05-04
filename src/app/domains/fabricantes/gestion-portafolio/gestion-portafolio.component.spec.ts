a20fa2a01063b062a7ab3f44cea6bce2c729f2a0
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-05-03 19:19:17 -0500

    149 - Se actualizan los imports en los archivos de pruebas unitarias para los diferentes componentes que usan la interfaz CategoriaProductos

--- src/app/domains/fabricantes/gestion-portafolio/gestion-portafolio.component.spec.ts
+++ src/app/domains/fabricantes/gestion-portafolio/gestion-portafolio.component.spec.ts
@@ -2,16 +2,33 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { GestionPortafolioComponent } from './gestion-portafolio.component';
+import { GestionPortafolioService } from './gestion-portafolio.service';
+import { FabricantesModule } from '../fabricantes.module';
 
 describe('GestionPortafolioComponent', () => {
   let component: GestionPortafolioComponent;
   let fixture: ComponentFixture<GestionPortafolioComponent>;
+  let service: GestionPortafolioService;
+  let httpMock: HttpTestingController;
+
+  let apiUrlSellers = environment.apiUrlSellers + `/sellers`;
+  let apiUrlProducts = environment.apiUrlProducts + `/products`;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ GestionPortafolioComponent ]
+      declarations: [ GestionPortafolioComponent ],
+      imports: [
+        FabricantesModule,
+        HttpClientTestingModule,
+      ],
+      providers:[
+        GestionPortafolioService
+      ]
     })
     .compileComponents();
   }));
@@ -19,10 +36,19 @@ describe('GestionPortafolioComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GestionPortafolioComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(GestionPortafolioService);
+    httpMock = TestBed.inject(HttpTestingController);
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    const req = httpMock.expectOne(`${environment.apiUrlProviders}/providers`);
+    req.flush({});
   });
 });
--- src/app/domains/fabricantes/gestion-portafolio/gestion-portafolio.service.spec.ts
+++ src/app/domains/fabricantes/gestion-portafolio/gestion-portafolio.service.spec.ts
@@ -1,16 +1,35 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, waitForAsync, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
+
+import { environment } from '../../../../environments/environment';
 import { GestionPortafolioService } from './gestion-portafolio.service';
 
 describe('Service: GestionPortafolio', () => {
+  let service: GestionPortafolioService;
+  let httpMock: HttpTestingController;
+
+  const apiUrl = environment.apiUrlSales + `/sales-plans/add`;
+  const apiUrlSellers = environment.apiUrlSellers + `/sellers`;
+  const apiUrlProducts = environment.apiUrlProducts + `/products`;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
       providers: [GestionPortafolioService]
     });
+
+    service = TestBed.inject(GestionPortafolioService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should ...', inject([GestionPortafolioService], (service: GestionPortafolioService) => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
 });
