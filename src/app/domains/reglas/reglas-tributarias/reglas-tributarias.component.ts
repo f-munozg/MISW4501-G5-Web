d79920c471a797436c465896fc9f9ad38db1e926
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-05-09 00:46:27 -0500

    169 - Se inicia la implementación del HTML del componente de reglas tributarias

--- src/app/domains/reglas/reglas-tributarias/reglas-tributarias.component.ts
+++ src/app/domains/reglas/reglas-tributarias/reglas-tributarias.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Paises, PaisesType2LabelMapping, TipoImpuesto, TipoImpuesto2LabelMapping } from '../reglas.model';
+
+export interface TableRow {
+  id: string;
+  pais: Paises;
+  tipo_impuesto: TipoImpuesto;
+  valor: number;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-reglas-tributarias',
@@ -7,6 +16,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reglas-tributarias.component.css']
 })
 export class ReglasTributariasComponent implements OnInit {
+  
+
+  public PaisesType2LabelMapping = PaisesType2LabelMapping;
+  public listaPaises = Object.values(Paises); 
+
+  public TipoImpuesto2LabelMapping = TipoImpuesto2LabelMapping;
+  public listaTiposImpuestos = Object.values(TipoImpuesto);
+
+  tableData: TableRow[] = [];
+
+  tableColumns = [
+    {
+      name: 'country',
+      header: 'País',
+      cell: (item: any) => item.pais.toString()
+    },
+    {
+      name: 'tax_type',
+      header: 'Tipo Impuesto',
+      cell: (item: any) => item.tipo_impuesto.toString()
+    },
+    {
+      name: 'tax_value',
+      header: 'Valor',
+      cell: (item: any) => item.valor.toString()
+    },
+  ];
+
+  visibleColumns = ['country', 'tax_type', 'tax_value'];
+
+  assignAction = [
+    {
+      icon: 'Editar',
+      tooltip: 'Editar',
+      action: (row: any) => {}
+    },
+    {
+      icon: 'Eliminar',
+      tooltip: 'Eliminar',
+      action: (row: any) => {}
+    }
+  ]
 
   constructor() { }
 
