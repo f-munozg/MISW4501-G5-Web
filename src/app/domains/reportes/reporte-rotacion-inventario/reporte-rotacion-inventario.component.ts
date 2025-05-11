46d23c115a504d1e0b6e45bfb0b309b3aef4ed05
madiazf1 <ma.diazf1@uniandes.edu.co>
2025-05-11 18:56:27 -0500

    196 - Se avanza en la implementación del HTML y CSS del componente

--- src/app/domains/reportes/reporte-rotacion-inventario/reporte-rotacion-inventario.component.ts
+++ src/app/domains/reportes/reporte-rotacion-inventario/reporte-rotacion-inventario.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TableRow{
+
+}
+
 @Component({
   selector: 'app-reporte-rotacion-inventario',
   standalone: false,
@@ -7,6 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reporte-rotacion-inventario.component.css']
 })
 export class ReporteRotacionInventarioComponent implements OnInit {
+
+  tableData: TableRow[] = [];
+
+  tableColumns = [
+    {
+      name: 'one',
+      header: 'one',
+      cell: (item: any) => item.one.toString()
+    },
+    {
+      name: 'two',
+      header: 'two',
+      cell: (item: any) => item.two.toString()
+    },
+    {
+      name: 'three',
+      header: 'three',
+      cell: (item: any) => item.three.toString()
+    },
+  ]
+
+  visibleColumns = ['one', 'two', 'three']
 
   constructor() { }
 
